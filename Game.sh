836a98294da153ebea542c71268c7d015bf0016a
sawantpalvi1234 <sawantpalvi087@gmail>
2020-08-23 17:14:48 +0530

    Added:Functionality to start game by resetting the board

--- Game.sh
+++ Game.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo "Welcome to Tic-Tac-Toe game"
+declare -A position
+for (( i=1 ; i<=9 ; i++ ))
+do
+	position[$i]=$i
+done
+echo " Refer cell number as the position of the board!"
+function board() {
+	echo " ${position[1]} | ${position[2]} | ${position[3]}"
+	echo " ${position[4]} | ${position[5]} | ${position[6]}"
+	echo " ${position[7]} | ${position[8]} | ${position[9]}"
+}
+board
